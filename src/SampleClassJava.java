672af626dd73a24858736d905244eec69664dc33
b1 <srikanth.chincholi@gmail.com>
2017-10-30 18:17:28 +0530

    added a line in master branch

--- src/SampleClassJava.java
+++ src/SampleClassJava.java
@@ -8,7 +8,7 @@ public class SampleClassJava {
 		System.out.println("second line added in eclipse");
 		
 		System.out.println("first line added in git hub");
-		
+		// line added below to see if change is made to master
 		
 	}
 
