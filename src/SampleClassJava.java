010ddebd2c221a12278feed6cafe536925f5f942
b1 <srikanth.chincholi@gmail.com>
2017-10-30 18:01:06 +0530

    lines edited

--- src/SampleClassJava.java
+++ src/SampleClassJava.java
@@ -4,7 +4,8 @@ public class SampleClassJava {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("line added in eclipse");
+		System.out.println("first line added in eclipse");
+		System.out.println("second line added in eclipse");
 	}
 
 }
